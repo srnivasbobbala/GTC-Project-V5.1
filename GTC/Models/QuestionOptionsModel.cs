dfc827cb87fb4603b86b3fe11acde6b82699a9f9
Manikanta <manik@SBLDSK-015>
2017-09-19 11:30:24 +0530

    Question options binded

---
+++ GTC/Models/QuestionOptionsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GTC.Models
+{
+    public class QuestionOptionsModel
+    {
+        public int Id { get; set; }
+        public QuestionModel question { get; set; }
+        public string Option { get; set; }
+
+    }
+}
