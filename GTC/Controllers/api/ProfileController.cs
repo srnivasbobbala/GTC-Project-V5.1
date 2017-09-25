747a723da243ebf3b5c53948bb7a1f053029dace
Manikanta <manik@SBLDSK-015>
2017-09-25 11:31:41 +0530

    Login api prepared

---
+++ GTC/Controllers/api/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using GTC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GTC.Controllers.api
+{
+    public class ProfileController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        [HttpPost]
+        [Route("api/Login/")]
+        public IHttpActionResult Login(Profile loginDetails)
+        {
+            return Ok(db.LoginUser(loginDetails));
+        }
+    }
+}
